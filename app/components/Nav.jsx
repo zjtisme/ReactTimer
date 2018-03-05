03870d52e469ba513c45e52d510db3e082fc5524
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-04 22:34:32 -0800

    project initialization

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+var React=require('react');
+var {Link, IndexLink}=require('react-router');
+
+var Nav=React.createClass({
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by <a href="#" target="_blank">Jintai</a></li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports=Nav;
