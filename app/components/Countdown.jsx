7bcc67f2d13c37d0307dbb5b30feb580c8baed43
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-05 15:36:02 -0800

    Create clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 var React=require('react');
+var Clock=require('Clock');
 
 var Countdown=React.createClass({
   render: function() {
     return (
       <div>
-        Countdown Page
+        <Clock totalSeconds={129}/>
       </div>
     );
   }
