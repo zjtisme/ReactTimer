6396486392ceade9fe8895a38a822df6fee6c239
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-05 00:06:25 -0800

    Add two components

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React=require('react');
+
+var Timer=React.createClass({
+  render: function() {
+    return (
+      <div>
+        Timer Page
+      </div>
+    );
+  }
+});
+
+module.exports=Timer;
